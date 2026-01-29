e4ffbb204b3ba324fbea240430ec6220cee4e02e
DTH235778-HuynhMInhThuan <thuan_dth235778@student.agu.edu.vn>
2026-01-29 10:00:00 +0700

    first commit

---
+++ Data/DienNuoc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhaTro.Data
+{
+    public class DienNuoc
+    {
+
+        [Key]
+        public int MaDN { get; set; }
+
+        public int MaPhong { get; set; }
+
+        public int ChiSoDienCu { get; set; }
+        public int ChiSoDienMoi { get; set; }
+
+        public int ChiSoNuocCu { get; set; }
+        public int ChiSoNuocMoi { get; set; }
+
+        public Phong Phong { get; set; }
+    }
+}
---
+++ Data/HoaDon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhaTro.Data
+{
+    public class HoaDon
+    {
+        [Key]
+        public int MaHoaDon { get; set; }
+
+        public int MaPhong { get; set; }
+
+        public int Thang { get; set; }
+        public int Nam { get; set; }
+
+        public int TienPhong { get; set; }
+        public int TienDien { get; set; }
+        public int TienNuoc { get; set; }
+
+        public int TongTien { get; set; }
+
+        public Phong Phong { get; set; }
+    }
+}
